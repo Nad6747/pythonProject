20278a53a700451226fdbab8cc03f6c4c3fabdcf
Kab_5_PC_2 <nm508545@gmail.com>
2024-02-13 21:20:47 +0300

    дата

--- lesson2.py
+++ lesson2.py
@@ -1,24 +1,45 @@
-from datetime import datetime
+# from datetime import datetime
+#
+# data =datetime.now()
+# time = data.strftime("%H:%M:%S")
+# day = datetime.date(data)
+# day2 = datetime.weekday(data)
+# if day2 == 0:
+#     day2 ="Понедельник"
+# elif day2 == 1:
+#     day2 ="Вторник"
+# elif day2 == 2:
+#     day2 = ("Среда")
+# elif day2 == 3:
+#     day2 ="Четверг"
+# elif day2 == 4:
+#     day2 ="Пятница"
+# elif day2 == 5:
+#     day2 ="Суббота"
+# elif day2 == 6:
+#     day2 ="Воскресенье"
+#
+# print(f"Сегодня у вас {day},что это {day2} и сейчас такое то время -{time}")
 
-data =datetime.now()
-time = data.strftime("%H:%M:%S")
-day = datetime.date(data)
-day2 = datetime.weekday(data)
-if day2 == 0:
-    day2 ="Понедельник"
-elif day2 == 1:
-    day2 ="Вторник"
-elif day2 == 2:
-    day2 = ("Среда")
-elif day2 == 3:
-    day2 ="Четверг"
-elif day2 == 4:
-    day2 ="Пятница"
-elif day2 == 5:
-    day2 ="Суббота"
-elif day2 == 6:
-    day2 ="Воскресенье"
+#
+# a = ["pavel","ivan","anton","piton"]
+# count = 0
+# for name in a:
+#     if name == "piton":
+#         print(name)
+#         print(count)
+#
+#     print(count)
+#     count += 1
+#
+#     print(a[3])
 
 print(f"Сегодня у вас {day},что это {day2} и сейчас такое то время -{time}")
+#
 
+print("Привет")
+input("Введите число")
+n = []
+print(n)
+while n = =
 
