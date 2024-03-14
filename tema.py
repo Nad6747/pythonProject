c2133fb952f082adb75d25b6aaaf3c22027c8016
Kab_5_PC_2 <nm508545@gmail.com>
2024-03-14 20:10:17 +0300

    хэш

--- tema.py
+++ tema.py
@@ -1,18 +1,19 @@
-def ploshad(a):
-    s = a * a
-    print ("Площадь квадрата:",s)
-    p = a * 4
-    print ("Периметр квадрата:",p)
+# def ploshad(a):
+#     s = a * a
+#     print ("Площадь квадрата:",s)
+#     p = a * 4
+#     print ("Периметр квадрата:",p)
+#
+# ploshad(16)
+# print("попытка 2")
+# ploshad(10)
+#
+#
+# def perimetr(h,k):
+#     s = h * k
+#     print ("Площадь прямоугольника:",s)
+#     p = (h + k) * 2
+#     print("Периметр прямоугольника:",p)
+#
+# perimetr(10,5)
 
-ploshad(16)
-print("попытка 2")
-ploshad(10)
-
-
-def perimetr(h,k):
-    s = h * k
-    print ("Площадь прямоугольника:",s)
-    p = (h + k) * 2
-    print("Периметр прямоугольника:",p)
-
-perimetr(10,5)
