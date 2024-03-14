8a6034b567cf8ce41548582a34d7810dfc301bb2
Kab_5_PC_2 <nm508545@gmail.com>
2024-03-14 21:21:23 +0300

    хэш

---
+++ peremena.py
@@ -0,0 +1,59 @@
+#number =5
+
+#number_user = int(input("Введи число от 2000 до 5000: "))
+
+#print(type(number_user))
+
+#str
+
+#print(type(number))
+
+# ooo = "45"
+# print(type(ooo))
+#
+# fint = 20
+# print(type(fint))
+#
+# ccc = str(47)
+# print(type(ccc))
+#
+# bond = 16
+# print(type(bond))
+#
+# osel = int (input("Введите чмсло"))
+# print(type(osel))
+
+
+# string_s = "Hello"
+#
+# print(string_s[-2])
+
+
+
+
+#
+# string_s = ("our jungle is very dense and impassable.There are many dangerous monsters in them")
+# #Вытащить посимвольно слово "turtle"
+# print(string_s[-4])
+# print(string_s[1])
+# print(string_s[2])
+# print(string_s[-4])
+# print(string_s[8])
+# print(string_s[9])
+
+
+# a = [1,1,2,3,5,8,13,21,34,55,89]
+# b = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+# #1,2,3,5,8,13
+#
+# c = []
+#
+# for i in  range(len(a)):
+#     if a[i] in b:
+#         c.append(a[i])
+# print(c)
+
+
+a = [1,1,2,3,5,8,13,21,34,55,89]
+for i in range(len (a)):
+    if a[i]< 5:
